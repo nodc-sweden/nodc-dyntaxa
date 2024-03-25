af3ec0ce70d54f112f6787f340cb20c3af149cd2
Wenzer Magnus <a001985@ad.smhi.se>
2024-03-25 13:10:25 +0100

    Add download config files func

--- src/nodc_dyntaxa/__init__.py
+++ src/nodc_dyntaxa/__init__.py
@@ -1,19 +1,25 @@
 import functools
+import logging
 import pathlib
-import sys
 
-from .dyntaxa_taxon import DyntaxaTaxon
-from .dyntaxa_whitelist import DyntaxaWhitelist
-from .translate_dyntaxa import TranslateDyntaxa
-from .red_list_species import RedListSpecies
+import requests
 
+from nodc_dyntaxa.dyntaxa_taxon import DyntaxaTaxon
+from nodc_dyntaxa.dyntaxa_whitelist import DyntaxaWhitelist
+from nodc_dyntaxa.red_list_species import RedListSpecies
+from nodc_dyntaxa.translate_dyntaxa import TranslateDyntaxa
 
-if getattr(sys, 'frozen', False):
-    THIS_DIR = pathlib.Path(sys.executable).parent
-else:
-    THIS_DIR = pathlib.Path(__file__).parent
+logger = logging.getLogger(__name__)
 
+THIS_DIR = pathlib.Path(__file__).parent
 CONFIG_DIR = THIS_DIR / 'CONFIG_FILES'
+
+CONFIG_URLS = [
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-dyntaxa/main/src/nodc_dyntaxa/CONFIG_FILES/translate_to_dyntaxa.txt',
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-dyntaxa/main/src/nodc_dyntaxa/CONFIG_FILES/dyntaxa_whitelist.txt',
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-dyntaxa/main/src/nodc_dyntaxa/CONFIG_FILES/Taxon.csv',
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-dyntaxa/main/src/nodc_dyntaxa/CONFIG_FILES/red_list_species.txt',
+]
 
 
 @functools.cache
@@ -41,3 +47,23 @@ def get_dyntaxa_taxon_object(filter_whitelist=True) -> "DyntaxaTaxon":
 def get_red_list_object() -> "RedListSpecies":
     path = CONFIG_DIR / 'red_list_species.txt'
     return RedListSpecies(path)
+
+
+def update_config_files() -> None:
+    """Downloads config files from github"""
+    try:
+        for url in CONFIG_URLS:
+            name = pathlib.Path(url).name
+            target_path = CONFIG_DIR / name
+            res = requests.get(url)
+            with open(target_path, 'w', encoding='utf8') as fid:
+                fid.write(res.text)
+                logger.info(f'Config file "{name}" updated from {url}')
+    except requests.exceptions.ConnectionError:
+        logger.warning('Connection error. Could not update config files!')
+
+
+if __name__ == '__main__':
+    update_config_files()
+
+
